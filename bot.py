f16f2b3413a0bae66df4773d4c42e76237ce4b42
Neo <gysi.krya@gmail.com>
2025-10-03 12:46:28 +0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import logging
+import asyncio
+import os
+from dotenv import load_dotenv
+from aiogram import Bot,Dispatcher
+from app.heandler import router
+
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
